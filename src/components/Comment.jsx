b6a3d08b89bcf5339c90a7a6d7d78641fe1c8d18
Nabin Dhakal <99952917+Nabin-Dhakal@users.noreply.github.com>
2025-07-15 22:25:35 +0545

    Update Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,7 +12,7 @@ const Comment = ({ onClose }) => {
   const [formData, setFormData] = useState({
     content: '',
   });
-    const [token] = useToken();
+   const [token, loading, error] = useToken();
 
 
   const fetchComments = async () => {

a49ae3f9ea2f069eb993e8d8f80d99fc996aec96
Nabin Dhakal <99952917+Nabin-Dhakal@users.noreply.github.com>
2025-07-15 23:00:03 +0545

    Update useToken.js

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import axios from "../api/axios";
 
 const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) {
+      return decodeURIComponent(value);
+    }
+  }
   return null;
 };
 
